ae1bfc0cb0e061e6b08dddd36efa60957f89a803
DanielSan666 <daniel.sanchez170298@gmail.com>
2024-08-15 11:11:11 -0600

    delete

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
+// Reemplaza `process.env.MONGO_URI` con tu cadena de conexión directamente
+const MONGO_URI = 'mongodb+srv://danielsanchez170298:1234@escuela.swndr.mongodb.net/?retryWrites=true&w=majority&appName=escuela';
+
 const connectDB = async () => {
+  if (!MONGO_URI) {
+    console.error('MongoDB URI is not defined');
+    process.exit(1);
+  }
+
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      // Elimina las opciones obsoletas
+    await mongoose.connect(MONGO_URI, {
+      // Opciones adicionales si es necesario
     });
     console.log('MongoDB connected');
   } catch (error) {
